77518559db62b8d666190714cb0afa48068b92ed
Taro Omiya <taro.omiya@omiyagames.com>
2017-05-04 21:03:00 -0400

    Final set of lessons.

---
+++ Assets/AnimatorEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorEditor : MonoBehaviour {
+    [SerializeField]
+    Animator controller;
+
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.Space))
+        {
+            controller.SetInteger("state", 2);
+        }
+        else
+        {
+            controller.SetInteger("state", 0);
+        }
+    }
+}
